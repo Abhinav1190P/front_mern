f32f12cf8d3167027f261ea040c948bd40fdd4eb
Abhinav <pandeysandeep1190@gmail.com>
2021-05-25 19:47:17 +0530

    ok

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
-    app.use(proxy('/api/', 
+    app.use(proxy('/', 
         { target: 'https://deploy-server1.herokuapp.com/' }
     ));
 }
