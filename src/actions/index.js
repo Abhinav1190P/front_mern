ff734c17a1852456ff99014de3abe1613adbe8ae
Abhinav <pandeysandeep1190@gmail.com>
2021-05-26 20:08:30 +0530

    szraw

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 
 export function postuser(data){
 
-const request = axios.post('https://deploy-server1.herokuapp.com/api/formPost',data)
+const request = axios.post('/api/formPost',data)
 .then((response)=>{
     return response.data
 })
 
-console.log(request)
+
 return{
 type:'user_post',
 payload:request
